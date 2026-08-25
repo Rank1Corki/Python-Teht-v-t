9917ba2ab6b1865045e3d1dcc7bfdac28e4504ee
Rank1Corki <s2200901@edu.bc.fi>
2026-08-25 13:06:24 +0300

    module 5 ja module 6 tehtävii

--- PeliProjekti/Peli.py
+++ PeliProjekti/Peli.py
@@ -1,4 +1,11 @@
 nimi = input("Mikä sinun nimesi on: ")
-ikä = input("Kuinka vanha olet: ")
+ikä = int(input("Kuinka vanha olet: "))
 
-print(f"Sinun nimesi on: {nimi} ja olet {ikä}")
+
+while True:
+    if ikä < 12:
+        print("Olet ala ikäinen.")
+        break
+    else:
+        print(f"Sinun nimesi on: {nimi} ja olet {ikä}")
+        break
