257fcd2958404e30da155eb87c3f5a38defd3ed7
Rank1Corki <s2200901@edu.bc.fi>
2026-09-04 18:13:19 +0300

    uudistuksia auto ja talo koodiin

--- Module10/Talo.py
+++ Module10/Talo.py
@@ -15,42 +15,35 @@ class Hissi:
             self.kerros -= 1
         print(f"Hissi on nyt {self.kerros} kerroksessa.")
 
-    def siirry_kerrokseen(self, kohde):
-        if kohde < self.alin or kohde > self.ylin:
-            print(f"Kerros {kohde} ei ole olemassa.")
-            return
-
-        while self.kerros < kohde:
-            self.kerros_ylos()
-
-        while self.kerros > kohde:
-            self.kerros_alas()
 
 class Talo:
-    his = Hissi(1, 10)
-    palohälytys = False
     def __init__(self,alin_kerros, ylin_kerros, hissien_lkm=5):
         self.alin = alin_kerros
         self.ylin = ylin_kerros
         self.hissit = []
-
         for _ in range(hissien_lkm):
             self.hissit.append(Hissi(self.alin, self.ylin))
+
+
 
     def aja_hissiä(self, hissin_numero, kohdekerros):
         if kohdekerros < self.alin or kohdekerros > self.ylin:
             print(f"Kerros {kohdekerros} ei ole olemassa.")
             return
-        while self.his.kerros < kohdekerros:
-            self.his.kerros_ylos()
 
-        while self.his.kerros > kohdekerros:
-            self.his.kerros_alas()
+        valittu_hissi = self.hissit[hissin_numero - 1]
+        while valittu_hissi.kerros < kohdekerros:
+            valittu_hissi.kerros_ylos()
+
+        while valittu_hissi.kerros > kohdekerros:
+            valittu_hissi.kerros_alas()
+
 
     def palohälytys(self):
-        self.palohälytys = True
-        while self.palohälytys == True:
-            self.his.siirry_kerrokseen(self.alin)
+        for i in range(len(self.hissit)):
+            hissin_nmr = i + 1 
+            self.aja_hissiä(hissin_nmr, self.alin)
+
 
 if __name__ == "__main__":
     talo = Talo(1, 10, 4)
@@ -59,5 +52,5 @@ if __name__ == "__main__":
     print("Palohälytys.")
     talo.palohälytys()
     print(f"Hissi on nyt alimmassa kerroksessa")
-
+    talo.aja_hissiä(1, 5)
 
